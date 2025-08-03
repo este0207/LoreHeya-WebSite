f06e4437f0e6d9d403a019fd68cdf7a1093e92a5
este0207 <esteban.h0207@gmail.com>
2025-08-03 17:02:52 +0200

    update github sh file

--- init-github.sh
+++ init-github.sh
@@ -2,9 +2,12 @@
 
 set -e
 
+echo "Entrez le message du commit :"
+read COMMIT_MSG
+
 git init
 git add .
-git commit -m "Initial commit"
+git commit -m "$COMMIT_MSG"
 git remote add origin https://github.com/este0207/LoreHeya-WebSite.git
 git branch -M main
 git push -u origin main
