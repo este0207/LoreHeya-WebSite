9bd99d045c4d78615808f2229a2b5b8db3eb2114
este0207 <esteban.h0207@gmail.com>
2025-08-09 18:29:59 +0200

    add biographie

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,6 +39,21 @@ export default function Page() {
   return (
     <main>
       <Header></Header>
+      <section className="section">
+        <div className="container home-story">
+          <h2>Mon parcours</h2>
+          <p className="home-story-content">{`Enfant je voyais l avenir, les défunts, ressentais les blessures des autres... sans comprendre qui j etais ni quoi faire de ces capacités. 
+
+Ce n'est que des annes plus tard que tout est devenu clair lorsque 4 chamans traditionnels m ont reconnu comme "chamane de naissance" m expliquant que certains ancetres chamans marquent un de leur descendant comme leur successeur.  Celui ci devra suivre une longue formation afin de perpétuer les traditions ancestrales. 
+
+C'est ainsi qu a commencé mon enseignement, en France et en Mongolie vers un chemin qu il me semble n avoir jamais quitté. 
+
+Aujourd'hui je suis dévouée aux autres afin de transmettre toutes ces connaissances de mes Maîtres Chamans en faisant le pont entre le pays berceau du chamanisme et la France qui a perdu ses traditions. 
+
+Je propose des formations a distance et en présence pour ceux qui sont désignés Chamans mais aussi a tous ceux qui souhaitent devenir praticien en chamanisme et/ou se guérir de leurs propres blessures,
+Des soins, des guidances, et des voyages vers la Mongolie afin de rencontrer des Chamans traditionnels. `}</p>
+        </div>
+      </section>
       <div className="CardContainer">
         <Card
           title="Platon"
